443e360569db5eca7627ffec131accac0db85156
David Keyes <david@rfortherestofus.com>
2019-11-07 10:51:52 -0800

    Initial commit

---
+++ 01-getting-started-exercises.R
@@ -0,0 +1,28 @@
+
+# Install Packages --------------------------------------------------------
+
+# Install the tidyverse and skimr packages using the install.packages function
+
+
+# Load Packages -----------------------------------------------------------
+
+# Load the tidyverse and skimr packages using the library function
+
+
+# Import Data -------------------------------------------------------------
+
+# Import the faketucky data into a data frame called faketucky.
+
+
+# Examine Data ------------------------------------------------------------
+
+# Enter the name of your data frame (faketucky) and run it as code to see the output.
+
+
+# Open faketucky by clicking on it in the environment/history pane
+
+
+# Use the skim() function to examine faketucky.
+
+
+s
